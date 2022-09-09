6c4ec09af054efb051277beaf6adec6182449167
PopovaElis <elizprest@gmail.com>
2022-09-09 10:09:57 +0500

    почти

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,30 @@
 
 public class Employee {
-    String fullName;
-    int department;
-    int salary;
+    private String fullName;
+    private int department;
+   private int salary;
+    private int id =0;
+
+    public Employee( String fullName,int department, int salary){
+
+        this.fullName=fullName;
+        this.department=department;
+        this.salary=salary;
+       this.id++;
+    }
+
+
+
+    public String getFullName(){return this.fullName;}
+    public int getDepartment(){return this.department;}
+    public int getSalary(){return this.salary;}
+    public int setDepartment(){return this.department;}
+    public int setSalary(){return this.salary;}
+
+    @Override
+    public String toString() {
+        return "id"+ id + " Имя- " + fullName +
+                " отдел- " + department +
+                " зп- " + salary;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
 public class Main {
+
+        private static final Employee[] employees= new Employee[10];
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee one = new Employee("Иванов Иван Иванович",1,80_000);
+        Employee two = new Employee("Иванова Лариса Ивановна",2,100_000);
+        Employee three = new Employee("Степанов Степан Степанович",1,60_000);
+
+        employees[0]=one;
+        employees[1]=two;
+        employees[2]=three;
+
+
+        System.out.println(employees[0]);
+        System.out.println(employees[1]);
+
     }
-}
+        public static void printEmployees() {
+            for (Employee employee: employees){
+                System.out.println(employee);
+            }
+        }
+
+            public static int calculateSalary() {
+        int sum= 0;
+        for (Employee employee : employees) {
+            sum += employee.getSalary();
+        }
+        return sum;
+            }
+
+    public static Employee maxMinSalary() {
+        Employee result=employees[0];
+        int maxSalary =employees[0].getSalary();
+        for (Employee employee: employees){
+            if (employee.getSalary()>maxSalary){
+                maxSalary = employee.getSalary();
+                result= employee;
+            }
+        }
+        return result;
+    }
+
+    public static float calculateAverageSalary(){
+            return calculateSalary()/(float) employees.length; }
+
+public static void printFullName(){
+            for (Employee employee: employees){
+                System.out.println(employee.getFullName());
+            }
+            }
+        }
+
