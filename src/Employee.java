832adb6974ab226c2a8eeda325dfa085a4cb9ed2
PopovaElis <elizprest@gmail.com>
2022-09-09 19:11:53 +0500

    попровила замечания

--- src/Employee.java
+++ src/Employee.java
@@ -2,31 +2,47 @@
 public class Employee {
     private String fullName;
     private int department;
-   private int salary;
-  private int id;
-  private static int counter;
+    private int salary;
+    private int id;
+    private static int counter = 1;
 
 
-    public Employee( String fullName,int department, int salary){
+    public Employee(String fullName, int department, int salary) {
 
-        this.fullName=fullName;
-        this.department=department;
-        this.salary=salary;
-        this.id= 1+counter++;
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        this.id = counter++;
     }
 
 
+    public String getFullName() {
+        return this.fullName;
+    }
 
-    public String getFullName(){return this.fullName;}
-    public int getDepartment(){return this.department;}
-    public int getSalary(){return this.salary;}
-    public int setDepartment(){return this.department;}
-    public int setSalary(){return this.salary;}
-    public int getId() {return this.id;}
+    public int getDepartment() {
+        return this.department;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public int setDepartment() {
+        return this.department;
+    }
+
+    public int setSalary() {
+        return this.salary;
+    }
+
+    public int getId() {
+        return this.id;
+    }
 
     @Override
     public String toString() {
-        return id+"."+" Имя- " + fullName +
+        return id + "." + " Имя- " + fullName +
                 " отдел- " + department +
                 " зп- " + salary;
     }
